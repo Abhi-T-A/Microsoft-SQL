0b34294b6da102619339a784717927edc4a34614
Abhi T A <abhi.t.a1806@gmail.com>
2025-10-02 18:39:22 +0530

    Add files via upload

---
+++ SQLQuery LIKE.sql
@@ -0,0 +1,21 @@
+use IBM
+
+--LIKE
+
+select * from employee
+
+select * from employee where name like 'a%'
+
+select * from employee where name like '%a'
+
+select * from employee where name like '_a%'
+
+select * from employee where name like '%a_'
+
+select * from employee where name like '%i%'
+
+select * from employee where name like '%it'
+
+select * from employee where age like '2%'
+
+select * from employee where name like '__h%'
