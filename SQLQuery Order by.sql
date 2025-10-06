dff481c2b600550e636c600cfc94e2e984da0787
Abhi T A <abhi.t.a1806@gmail.com>
2025-10-06 17:52:43 +0530

    Add files via upload

---
+++ SQLQuery Order by.sql
@@ -0,0 +1,23 @@
+Use IBM
+
+--Order by
+
+select * from employee
+
+
+select * from employee order by name 
+
+select * from employee order by age
+
+select * from employee order by name desc
+
+select * from employee order by age desc
+
+select * from employee order by department, id
+
+select * from employee order by gender, name
+
+select * from employee order by department, id desc
+
+
+select * from employee order by department desc, id desc
